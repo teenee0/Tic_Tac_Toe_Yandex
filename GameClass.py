83c2d3aea2f3b911a6b32f825b8213ae54650fb4
teenee0 <qwerty.gogi@mail.ru>
2024-10-15 16:53:38 +0300

    Add checking for field occupancy and bug with the dioganal check

--- GameClass.py
+++ GameClass.py
@@ -6,10 +6,15 @@ class TicTacToe:
 
 
     def make_move(self, row, col):
-        self.board[row][col] = self.player
+        if self.board[row][col] == " ":
+            self.board[row][col] = self.player
+        else:
+            self.switch_player()
+            print("Эта клетка занята!")
 
 
     def check_winner(self):
+
         for i in range(3):
             if (self.board[i][0] == self.board[i][1] == self.board[i][2] == self.player or
                     self.board[0][i] == self.board[1][i] == self.board[2][i] == self.player):
@@ -19,10 +24,14 @@ class TicTacToe:
                 return f"{self.player} won"
 
         if (self.board[0][0] == self.board[1][1] == self.board[2][2] == self.player or
-                self.board[0][2] == self.board[1][1] == self.board[0][2] == self.player):
+                self.board[0][2] == self.board[1][1] == self.board[2][0] == self.player):
 
             self.clear_board()
             return f"{self.player} won"
+
+        if " " not in "".join(["".join(row) for row in self.board]):
+            self.clear_board()
+            return "Ничья"
         else:
             self.switch_player()
             return f"Играем"
